84f5f0c9858ea59f2f2bf792c1b0c8e7b1d5e7ba
Huy Hoang <huythieuhoang@gmail.com>
2024-10-27 21:07:33 +1100

    Initial setup for the micro_batching gem

---
+++ lib/micro_batching.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "micro_batching/version"
+
+module MicroBatching
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/micro_batching_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe MicroBatching do
+  it "has a version number" do
+    expect(MicroBatching::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
